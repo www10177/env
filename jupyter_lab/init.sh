3c8bb8d865a8281c7b6cfc19184f816111f093f7
Rist <www10177@gmail.com>
2021-07-09 07:14:29 +0000

    modified jupyter lab config

--- jupyter_lab/init.sh
+++ jupyter_lab/init.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 pip3 install jupyterlab
 pip3 install ipython
-jupyter nbextension enable --py widgetsnbextension
-cp ./jupyter_lab_config.py ~/.jupyter/
+jupyter nbextension install --py widgetsnbextension --user
+jupyter nbextension enable widgetsnbextension --user --py
+cp ./jupyter_lab_config.py ~/.jupyter/ 
 ipython profile create
 cp ./ipython_kernel_config.py ~/.ipython/profile_default/
+
+jupyter labextension install jupyterlab-theme-solarized-dark
+pip3 install --upgrade jupyterlab-vim
+jupyter labextension install @jupyterlab/toc
+pip3 install ipympl ipython-autotime
+jupyter labextension install @jupyter-widgets/jupyterlab-manager jupyter-matplotlib
+jupyter labextension install @krassowski/jupyterlab_go_to_definition  
 

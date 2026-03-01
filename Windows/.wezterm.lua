a540141eb93bb99be29ab3fab364012b7bf6d131
Rist <www10177@gmail.com>
2026-03-01 23:12:18 +0800

    Add WezTerm configuration for WSL and key binding

---
+++ Windows/.wezterm.lua
@@ -0,0 +1,16 @@
+local wezterm = require 'wezterm'
+local act = wezterm.action
+
+
+return {
+  -- 指定預設啟動 WSL
+  default_prog = { 'wsl.exe', '~' },
+  keys = {
+    -- 按下 Alt + F1，自動在終端機輸入 qweasd
+    {
+      key = 'F1',
+      mods = 'ALT',
+      action = act.SendString 'hello world',
+    },
+  },
+}
